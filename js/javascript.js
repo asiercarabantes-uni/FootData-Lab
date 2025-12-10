8bcb2bcbfc0f0f5b6ff53b2f1aaeb6c0fd567794
Asier Carabantes <carabantes.asier@uni.eus>
2025-12-10 15:43:09 +0100

    feat: create a function to list the teams of each league

--- js/javascript.js
+++ js/javascript.js
@@ -1,5 +1,4 @@
 
-
 // load the content dynamically
 function loadContent(url) {
     fetch(url)
@@ -7,5 +6,39 @@ function loadContent(url) {
     .then(html => {
         document.getElementById('content').innerHTML = html;
     })
-    .catch(err => console.error('Error al cargar contenido:', err));
+    .catch(err => console.error('Error loading the content:', err));
 }
+
+// lists the teams of each league
+function listTeams(league, year) {
+    const url = `https://raw.githubusercontent.com/openfootball/football.json/master/${year}/${league}.json`;
+    
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        //console.log(data);
+        const matches = data.matches;
+        //console.log(matches);
+        const teamsSet = new Set();
+        for (const match of matches) {
+            if((match.round === "1. Round") || (match.round === "Matchday 1")) { // (loop once per league (round 1 / matchday 1) is enough)
+                teamsSet.add(match.team1);
+                teamsSet.add(match.team2);
+            } else break;
+        }
+        console.log(teamsSet);
+    })
+    .catch(err => console.error('Error loading the content:', err));
+}
+
+// get the total amount of points in the league
+function sumPoints() {
+    
+}
+
+// goals for, goals against and goal difference
+function goalsPerTeam()  {
+
+}
+
+// wins, draws and losses
